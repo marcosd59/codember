64a3f48bbaca6ba4d9fd48aa54dddced1882c52d
Marcos Damián Pool Canul <damian.marcospool@gmail.com>
2024-01-11 11:14:00 -0500

    Add files via upload

---
+++ codember 2023/CHALLENGE_01.js
@@ -0,0 +1,44 @@
+const mensajeEnMinusculas =
+  "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon";
+
+const mensaje = mensajeEnMinusculas.toLowerCase();
+
+var arr = [];
+
+arr = mensaje.split(" ");
+
+const res = arr.reduce((p, c) => {
+  c.split(" ").forEach((e) => {
+    p.set(e, p.has(e) ? p.get(e) + 1 : 1);
+  });
+
+  return p;
+}, new Map());
+
+// console.log([...res]); // <-- Todas, con las ocurrencias
+
+const unicas = [];
+const repetidas = [];
+
+res.forEach((v, k) => (v === 1 ? unicas.push(k) : repetidas.push(k)));
+
+// console.log(unicas); // <-- Palabras que no se repiten
+// console.log(repetidas); // <-- Palabras repetidas
+
+const todasLasPalabras = [...unicas, ...repetidas];
+
+const resObj = {};
+
+res.forEach((v, k) => (resObj[k] = v));
+
+const valoresNumericos = Array.from(res.values());
+
+const resultado = [];
+
+for (let i = 0; i < todasLasPalabras.length; i++) {
+  resultado.push(todasLasPalabras[i] + valoresNumericos[i]);
+}
+
+const cadenaResultante = resultado.join("");
+
+console.log(cadenaResultante);
